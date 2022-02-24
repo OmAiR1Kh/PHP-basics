844e442e81c8bce59593ba93d4655f75ad9b0985
Omair <abd.alkhahhar@gmail.com>
2022-02-24 13:32:32 +0200

    done till step 6

--- index.php
+++ index.php
@@ -6,6 +6,27 @@
     $height = 6;
     $width = 3;
     $area = $height * $width;
+    $position = 'student';
+    $github_URL = 'https://github.com/OmAiR1Kh';
+    $course_name = 'PHP';
+    $enrolled_student = 34;
+    $price = 30.5;
+    $on_discount = true;
+    if($on_discount == 1)
+    $on_discount = "yes";
+    else
+    $on_discount = "no";
     echo "<h2>My name is " . $name . " and my Email is" . $email . "</h2>";
-    echo $area
+    echo $area;
+    // echo "<p> My name is " $name  " and I'm a " . $position . "Please check my github URL " . $github_URL . "</p>";
+    echo "<br>";
+    echo "My name is $name and I'm a $position  Please check my github URL $github_URL";
+    echo "<br>";
+    echo "Course name : $course_name";
+    echo "<br>";
+    echo "Enrolled student : $enrolled_student";
+    echo "<br>";
+    echo "Course Price : $price$";
+    echo "<br>";
+    echo "Course on discount : $on_discount";
 ?>
---
+++ step6.php
@@ -0,0 +1,9 @@
+<?php
+    $height = 0;
+    $width = 0;
+    function calculateArea($height, $width) {
+        $area = $height * $width;
+        echo $area;
+    };
+    calculateArea(5,5)
+?>
