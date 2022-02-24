0eb34bfdb141211a371212f86d299943aa06cc39
Omair <abd.alkhahhar@gmail.com>
2022-02-24 12:28:45 +0200

    steps 1-> 3

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php 
+    echo "<p>Hello world, Help me I am using PHP</p>";
+    echo "I am getting errors but I can't see the errors";
+    $name = 'Omair';
+    $email = 'Abd.alkhahhar@gmail.com';
+    $height = 6;
+    $width = 3;
+    $area = $height * $width;
+    echo "<h2>My name is " . $name . " and my Email is" . $email . "</h2>";
+    echo $area
+?>
